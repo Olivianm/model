67aa5fb78f9bc4b548baf0f2828ba458992a1fe1
Olivianm <144073206+Olivianm@users.noreply.github.com>
2025-05-10 22:35:28 +0000

    name

--- app.py
+++ app.py
@@ -101,6 +101,6 @@ def predict():
             "status": "error"
         }), 500
 
-if _name_ == "_main_":
+if __name__ == "_main_":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
